20fe94068400fbd583d17d1d309f2bc6341b8079
adamman722 <tarpeyadam@gmail.com>
2022-03-29 23:51:11 -0400

    super tired

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import styled from "styled-components";
 function CountryCard() {
   const countries = useSelector((state) => state.countries.value);
-  if (countries.length > 249) {
-    console.log(countries);
-  }
-  return <div>CountryCard</div>;
+  console.log(countries[0]);
+
+  const MainFlex = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+
+  const FlexDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    border: 5px solid;
+    width: fit-content;
+    border-radius: 10px;
+  `;
+
+  const listOfFilterted = countries.map((country) => {
+    return (
+      <FlexDiv className="">
+        <img src={country.flags.png} alt="" />
+        <div>
+          <div>{country.name.common}</div>
+        </div>
+      </FlexDiv>
+    );
+  });
+
+  return (
+    <MainFlex>{countries.length === 250 ? listOfFilterted : null}</MainFlex>
+  );
 }
 
 export default CountryCard;
